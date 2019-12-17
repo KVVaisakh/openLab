ecfc647dcb9610d4316ac0bb1e8c61eed4b980b1
KVVaisakh <36007302+KVVaisakh@users.noreply.github.com>
2019-12-17 13:14:28 +0530

    Update lab3_u4cse17342_VaisakhKV.py

--- lab3/lab3_u4cse17342_VaisakhKV.py
+++ lab3/lab3_u4cse17342_VaisakhKV.py
@@ -22,7 +22,7 @@ cv2.imshow("img",avimg)
 cv2.waitKey(5000)
 cv2.destroyAllWindows()
 
-#3
+#3a
 import numpy
 
 img =cv2.imread("vaishak.jpg")
@@ -38,4 +38,42 @@ for i in range(dim[0]):
 
 cv2.imshow("img",imgt)
 cv2.waitKey(5000)
+cv2.destroyAllWindows()
+
+#3b
+import numpy
+import math 
+
+img =cv2.imread("vaishak.jpg")
+dim = img.shape
+
+a=math.pi/4
+imgt= numpy.zeros((int((dim[0]+dim[1])*math.cos(a)),int((dim[0]+dim[1])*math.sin(a)),3), numpy.uint8)
+
+mat=numpy.array([[math.cos(a),-math.sin(a)],[math.sin(a),math.cos(a)]])
+pos=numpy.zeros((dim[0]*dim[1],2), numpy.uint16)
+
+k=0
+for i in range(dim[0]):
+	for j in range(dim[1]):
+		pos[k]=[i,j]
+		k+=1
+
+newPos=numpy.dot(pos,mat)
+newPos=newPos.astype(int)
+
+minx=newPos.min(axis=0)
+for i in range(k):
+	# newPos[i][0]+=(-minx[0])
+	newPos[i][1]+=(-minx[1])
+	k-=1
+
+k=0
+for i in range(dim[0]):
+	for j in range(dim[1]):
+		imgt[newPos[k][0]][newPos[k][1]]=img[i][j]
+		k+=1
+
+cv2.imshow("img",imgt)
+cv2.waitKey(0)
 cv2.destroyAllWindows()

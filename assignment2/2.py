cb6df8f9d72279e203c1cf287f691aa6963c5112
Vaisakh K V <36007302+KVVaisakh@users.noreply.github.com>
2020-04-19 04:35:45 +0530

    Add files via upload

--- assignment2/2.py
+++ assignment2/2.py
@@ -2,8 +2,27 @@ import cv2
 import numpy
 
 org = cv2.imread("logndlinear.jpg",0)
-m=numpy.max(org)				# m[1] will have max pixel value
+m=numpy.max(org)				# m[1] will have max pixelel value
 c=255/(numpy.log(1+m))
 logImage = numpy.array(c*numpy.log(1+org),dtype=numpy.uint8)
 
 cv2.imwrite("logq2.jpg",logImage)
+
+def piecewise(img,r1,s1,r2,s2):
+	if (0 <= img and img <= r1):
+		return (s1 / r1)*img
+	elif (r1 < img and img <= r2):
+		return ((s2 - s1)/(r2 - r1)) *  (img - r1) + s1
+	else:
+		return ((255 - s2)/(255 - r2)) *  (img - r2) + s2
+r1 = 70
+s1 = 0
+r2 = 140
+s2 = 255
+
+vf=numpy.vectorize(piecewise)
+org = vf(org,r1,s1,r2,s2)
+
+cv2.imwrite("logq2b.jpg",org)
+cv2.waitKey(0)
+cv2.destroyAllWindows()

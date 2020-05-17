93787ceb48f82c7c01b9de2cd373edb71f5bfc17
Vaisakh K V <36007302+KVVaisakh@users.noreply.github.com>
2020-05-17 09:50:28 +0530

    Add files via upload

---
+++ lab9/9.py
@@ -0,0 +1,86 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+##1
+# img = cv2.imread('noisy_leaf.jpg',0)
+#
+#
+# blur = cv2.GaussianBlur(img, (5,5), 0)
+# ret, imgf = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+# cv2.imwrite('thresholded.jpg',imgf)
+#
+# plt.subplot(3,1,1), plt.imshow(img,cmap = 'gray')
+# plt.title('Original Noisy Image'), plt.xticks([]), plt.yticks([])
+# plt.subplot(3,1,2), plt.hist(img.ravel(), 256)
+# plt.axvline(x=ret, color='r', linestyle='dashed', linewidth=2)
+# plt.title('Histogram'), plt.xticks([]), plt.yticks([])
+# plt.subplot(3,1,3), plt.imshow(imgf,cmap = 'gray')
+# plt.title('Otsu thresholding'), plt.xticks([]), plt.yticks([])
+# plt.show()
+
+##2
+# img = cv2.imread('page.jpg',0)
+# img = cv2.medianBlur(img,5)
+#
+# ret,th1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
+# th2 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,11,2)
+# th3 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
+# cv2.imwrite('global_thresholded.jpg',th1)
+# cv2.imwrite('mean_thresholded.jpg',th2)
+# cv2.imwrite('gaussian_thresholded.jpg',th3)
+#
+# titles = ['Original Image', 'Global Thresholding (v = 127)',
+#             'Adaptive Mean Thresholding', 'Adaptive Gaussian Thresholding']
+# images = [img, th1, th2, th3]
+#
+# for i in range(4):
+#     plt.subplot(2,2,i+1),plt.imshow(images[i],'gray')
+#     plt.title(titles[i])
+#     plt.xticks([]),plt.yticks([])
+# plt.show()
+
+##3
+
+# img = cv2.imread('messi.jpg')
+# mask = np.zeros(img.shape[:2],np.uint8)
+#
+# bgdModel = np.zeros((1,65),np.float64)
+# fgdModel = np.zeros((1,65),np.float64)
+#
+# rect = (50,50,450,290)
+# cv2.grabCut(img,mask,rect,bgdModel,fgdModel,5,cv2.GC_INIT_WITH_RECT)
+#
+# mask2 = np.where((mask==2)|(mask==0),0,1).astype('uint8')
+# img = img*mask2[:,:,np.newaxis]
+# cv2.imwrite("croppedMessi.jpg",img)
+#
+# plt.imshow(img),plt.colorbar(),plt.show()
+
+##4
+import numpy as np
+import cv2
+
+img = cv2.imread('building.png')
+Z = img.reshape((-1,3))
+
+Z = np.float32(Z)
+
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+K = 8
+ret,label,center=cv2.kmeans(Z,K,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
+K = 3
+ret3,label3,center3=cv2.kmeans(Z,K,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
+
+center = np.uint8(center)
+res = center[label.flatten()]
+res2 = res.reshape((img.shape))
+
+center3 = np.uint8(center3)
+res3 = center3[label3.flatten()]
+res23 = res3.reshape((img.shape))
+
+cv2.imwrite('k=3building.png',res23)
+cv2.imwrite('k=8building.png',res2)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
